d324ed247996350cacd4e03ae731b76f1e554d25
Jay Idrees <idreesjay@gmail.com>
2021-06-30 15:48:03 -0400

    Added routes files and updated code in app.js, routes working. Also implemented serving static files using node.JS

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const path=require('path');
 
 const express=require('express');
 const bodyParser=require('body-parser');
-const { urlencoded } = require('body-parser');
 
 const app=express();
 
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin', adminRoutes);
+// Note that if you are redirecting to the home page'/' then you do not have to specify it- its already covered in the ShopRoutes file
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+
+
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -9,3 +9,10 @@ router.get('/add-product',(req,res,next)=>{
 res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
+
+router.post('/add-product',(req,res,next)=>{
+  console.log(req.body)
+  res.redirect('/');
+});
+
+module.exports=router;
